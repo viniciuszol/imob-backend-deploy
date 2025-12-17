db6adebc31e41fd1a48c665757e8a3b3bfc03bf8
viniciuszol <devvinicius08@gmai.com>
2025-12-17 12:53:25 -0300

    Update in Main

--- app/main.py
+++ app/main.py
@@ -37,6 +37,7 @@ app = FastAPI(title="ImobInvest API")
 origins = [
     "http://localhost:5173",   # Vite frontend
     "http://127.0.0.1:5173"
+    "https://main.d2byrs9y98woub.amplifyapp.com"
 ]
 
 app.add_middleware(
